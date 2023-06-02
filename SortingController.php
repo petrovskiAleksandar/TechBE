c8998311414eca7389c5c2ca219d8c3c626a051a
Aleksandar <aleksandar@embedsocial.com>
2023-06-02 16:52:27 +0200

    refactored version that only uses one for cycle

--- SortingController.php
+++ SortingController.php
@@ -7,7 +7,8 @@
     $orderByDate = '';
     $minimumRating = 1;
 
-    if (isset($_POST['filter'])) {
+    if (isset($_POST['filter']))
+    {
         echo $_POST['prioritizeByText'] . '<br/>';
         $prioritizeByText = $_POST['prioritizeByText'];
         echo $_POST['orderByRating'] . '<br/>';
@@ -18,12 +19,13 @@
         $minimumRating = (int) $_POST['minimumRating'];
     }
 
-    $reviews = array_filter($reviews, function ($review) use ($minimumRating){
+    $reviews = array_filter($reviews, function ($review) use ($minimumRating) {
         return (int)$review['rating'] >= $minimumRating;
     });
     $reviews = array_values($reviews);
 
-    $sortByRating = function ($first, $second) use ($orderByRating){
+    $sortByRating = function ($first, $second) use ($orderByRating)
+    {
         if ($orderByRating === 'Highest first') {
             return $first['rating'] < $second['rating'];
         }
@@ -31,72 +33,107 @@
         return $first['rating'] > $second['rating'];
     };
 
-    $sortByText = function ($first, $second) use ($prioritizeByText) {
+    $sortByText = function ($first, $second) use ($prioritizeByText)
+    {
         if ($prioritizeByText === 'Yes') {
             return strlen($second['reviewText']) > 0 && strlen($first['reviewText']) === 0;
         }
     };
 
-    $sortByDate = function ($first, $second) use ($orderByDate) {
+    $sortByDate = function ($first, $second) use ($orderByDate)
+    {
         if ($orderByDate === 'Newest first') {
-            return $second['reviewCreatedOnDate'] > $first['reviewCreatedOnDate'];
+            return $first['reviewCreatedOnTime'] < $second['reviewCreatedOnTime'];
         }
 
-        return $second['reviewCreatedOnDate'] < $first['reviewCreatedOnDate'];
+        return $first['reviewCreatedOnTime'] > $second['reviewCreatedOnTime'];
     };
 
-    function sortArray ($reviews, $condition) {
-        $localReviews = $reviews;
+    $sortIfRatingIsEqual = function ($first, $second) use ($sortByDate) {
+        $first['rating'] === $second['rating'] && $sortByDate($first, $second);
+    };
+    
+    $sortFunction = function ($first, $second) use ($prioritizeByText, $sortByRating, $sortIfRatingIsEqual)
+    {
+        if ($prioritizeByText === 'Yes') {
+            return (
+                (
+                    strlen($second['reviewText']) > 0 &&
+                    strlen($first['reviewText']) > 0 &&
+                    (
+                        $sortIfRatingIsEqual($first, $second) ||
+                        $sortByRating($first, $second)
+                    )
+                ) ||
+                (
+                    strlen($second['reviewText']) === 0 &&
+                    strlen($first['reviewText']) === 0 &&
+                    (
+                        $sortIfRatingIsEqual($first, $second) ||
+                        $sortByRating($first, $second)
+                    )
+                ) ||
+                (
+                    strlen($second['reviewText']) > 0 &&
+                    strlen($first['reviewText']) === 0
+                )
+            );
+        }
 
-        for ($i = 0; $i < count($localReviews); $i++)
+        return (
+                $sortIfRatingIsEqual($first, $second) ||
+                $sortByRating($first, $second)
+            ) ||
+            $sortByRating($first, $second);
+    };
+
+    for ($i = 0; $i < count($reviews); $i++)
+    {
+        $swapped = false;
+
+        for ($j = 0; $j < count($reviews) - $i - 1; $j++)
         {
-            $swapped = false;
+            if ($sortFunction($reviews[$j], $reviews[$j + 1]))
+            {
+                $temp = $reviews[$j];
+                $reviews[$j] = $reviews[$j + 1];
+                $reviews[$j + 1] = $temp;
 
-            for ($j = 0; $j < count($localReviews) - $i - 1; $j++) {
-                if ($condition($localReviews[$j], $localReviews[$j + 1])) {
-                    $temp = $localReviews[$j];
-                    $localReviews[$j] = $localReviews[$j + 1];
-                    $localReviews[$j + 1] = $temp;
-
-                    $swapped = true;
-                }
-            }
-
-            if ($swapped === false) {
-                break;
+                $swapped = true;
             }
         }
 
-        return $localReviews;
+        if ($swapped === false)
+        {
+            break;
+        }
     }
 
-    $reviews = sortArray($reviews, $sortByDate);
-    $reviews = sortArray($reviews, $sortByRating);
-    $reviews = sortArray($reviews, $sortByText);
 
-    // echo <<<TEXT
-    // <table style="padding: 100px">
-    //     <tr>
-    //         <th>Rating</th>
-    //         <th>Review Text</th>
-    //         <th>Created on</th>
-    //     </tr>
-    // TEXT;
+    echo <<<TEXT
+    <table style="padding: 100px">
+        <tr>
+            <th>Rating</th>
+            <th>Review Text</th>
+            <th>Created on</th>
+        </tr>
+    TEXT;
 
-    foreach ($reviews as $review) {
+    foreach ($reviews as $review)
+    {
         $reviewRating = $review['rating'];
         $reviewText = $review['reviewText'];
         $reviewDate = $review['reviewCreatedOnTime'];
 
-        // echo <<<TEXT
-        //         <tr style="text-align: center">
-        //             <td style="width: 100px;">$reviewRating</td>
-        //             <td style="width: 100px;">$reviewText</td>
-        //             <td style="width: 200px;">$reviewDate</td>
-        //         </tr>
-        // TEXT;
-        echo $review['rating'] . ' - ' .$review['reviewText'] .$review['reviewCreatedOnTime'] .'<br>';
+        echo <<<TEXT
+                <tr style="text-align: center">
+                    <td style="width: 100px;">$reviewRating</td>
+                    <td style="width: 100px;">$reviewText</td>
+                    <td style="width: 200px;">$reviewDate</td>
+                </tr>
+        TEXT;
+
     }
 
-    // echo '</table>';
+    echo '</table>';
 ?>
